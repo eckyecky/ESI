f6304e73fef1cbfaad43463e491876359669bf30
eckyecky <eckyecky@gmail.com>
2012-08-28 19:56:10 +1000

    Timer now prints temp

--- arduino/timer555/timer555.ino
+++ arduino/timer555/timer555.ino
@@ -1,3 +1,9 @@
+#define CAPACITANCE (1e-6)
+#define RESISTOR1 (5e3)
+#define BETA (3988.0)
+#define RINF (10e3*exp(-BETA/298.15))
+#define PERIODS 10
+
 long int t=0;
 long int t1;
 void setup(){
@@ -5,14 +11,22 @@ delay(1000);
 Serial.begin(9600);
 pinMode(2,INPUT);}
 void loop(){
+  float thermistorResistance;
+  float temp;
+  float period;
+  int i;
   while(digitalRead(2)==HIGH){};
-  while(digitalRead(2)==LOW){};
-  t= micros();
-  while(digitalRead(2)==HIGH){};
-  while(digitalRead(2)==LOW){};
+    while(digitalRead(2)==LOW){};
+    t=micros();
+  for (i=0;i<PERIODS;i++) {
+    while(digitalRead(2)==HIGH){};
+    while(digitalRead(2)==LOW){};
+  }
   t=micros()-t;
-  t1=millis();
-  //Serial.print(t1);Serial.print(" ");
-  Serial.println(t);
+  period=t/PERIODS;
+  thermistorResistance=(period*1e-6)/(2.0*log(2.0)*CAPACITANCE)-RESISTOR1/2.0;
+  temp=BETA/log(thermistorResistance/RINF)-273.15;
+
+  Serial.println(temp);
   delay(500);
 }
--- matlab/timer555read.m
+++ matlab/timer555read.m
@@ -10,7 +10,7 @@ rinf=10e3*exp(-B/298.15);
 
 C=1e-6;
 r_1=5e3;
-s1 = serial('COM3');    % define serial port
+s1 = serial('COM5');    % define serial port
 s1.BaudRate=9600;               % define baud rate
 set(s1, 'terminator', 'LF');    % define the terminator for println
 fopen(s1);
