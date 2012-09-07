6d7fab69db79e40985a03d80de2ce40ea6f44a49
docVed <berton.dj@gmail.com>
2012-09-07 16:54:24 +1000

    Neaten up code
    Changed some variable names
    Added comments, whitespace
    #defined some magic numbers
    moved t , t1 to local variables

--- arduino/timer555/timer555.ino
+++ arduino/timer555/timer555.ino
@@ -1,32 +1,40 @@
+// defining various sensor circut constants
 #define CAPACITANCE (1e-6)
 #define RESISTOR1 (5e3)
 #define BETA (3988.0)
 #define RINF (10e3*exp(-BETA/298.15))
-#define PERIODS 10
+#define CELCIUS_OFFSET 273.15
 
-long int t=0;
-long int t1;
+#define PERIODS 10 // number of clock periods to average 
+
 void setup(){
-delay(1000);
-Serial.begin(9600);
-pinMode(2,INPUT);}
+  delay(1000);
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+}
 void loop(){
-  float thermistorResistance;
+
+  long int t=0 ,t1;
+  float Rt; // thermistor resistance
   float temp;
   float period;
   int i;
-  while(digitalRead(2)==HIGH){};
-    while(digitalRead(2)==LOW){};
-    t=micros();
+
+  // read average period
+  while(digitalRead(2)==HIGH) {}; 
+  while(digitalRead(2)==LOW ) {};
+  t = micros();
+
   for (i=0;i<PERIODS;i++) {
-    while(digitalRead(2)==HIGH){};
-    while(digitalRead(2)==LOW){};
+    while(digitalRead(2)==HIGH) {};
+    while(digitalRead(2)==LOW ) {};
   }
-  t=micros()-t;
-  period=t/PERIODS;
-  thermistorResistance=(period*1e-6)/(2.0*log(2.0)*CAPACITANCE)-RESISTOR1/2.0;
-  temp=BETA/log(thermistorResistance/RINF)-273.15;
+
+  t = micros()-t;
+  period = t/PERIODS*1e-6; //converts to average and from (microsec) to (sec)
+  Rt = (period)/(2.0*log(2.0)*CAPACITANCE)-RESISTOR1/2.0;
+  temp = BETA/log(Rt/RINF) - CELCIUS_OFFSET ;
 
   Serial.println(temp);
-  delay(500);
+  delay(500); // used to limit serial output
 }
